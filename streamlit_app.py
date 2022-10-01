f133b7d1d96ee883a189f1d93e33966240d90043
ishani856 <114810181+ishani856@users.noreply.github.com>
2022-10-01 18:15:47 +0530

    Update streamlit_app.py
    Writtten my description with some commannds of streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
 streamlit.title('I am getting better version of myself every day')
+
+streamlit.header('I have started my journey and in the middle of it to  being an data scientist')
+streamlit.text('I fulfill the requirements and skillsets of above headline :-) hehehe')
+streamlit.text('Like data analysis of the streaming data, python as a communicator in the ground of world, visualisation as an reporting , algorithms to train and deploy the model')
+streamlit.text('I have some experience in data engineering tools like ssis,ssms,ssas,ssrs,databricks pipeline')
+streamlit.text('Yeaahhhh.....cool so for these  sql is must and ETL related practices as well')
+streamlit.text('Also completed the Full Stack Analyst Certification')
+streamlit.text('A good practice of On Cloud Practices in Azure is been added inspite of all above ...... sounds good yeaahhhhh .......... lets keep going this journey')
+
